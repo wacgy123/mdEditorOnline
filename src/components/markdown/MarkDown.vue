863776a226ed315b04bc6566935165e439261bf0
llm <luoqin123@qq.com>
2021-01-14 20:34:35 +0800

    a

---
+++ src/components/markdown/MarkDown.vue
@@ -0,0 +1,28 @@
+<template>
+ <div>
+     <mavon-editor></mavon-editor>
+ </div>
+</template>
+
+<script>
+import {mavonEditor} from "mavon-editor"
+import 'mavon-editor/dist/css/index.css'
+ export default {
+   name:'markdown',
+   components: {
+       mavonEditor
+   },
+   data () {
+     return {
+     }
+   },
+   mounted () {
+     
+   }
+ }
+</script>
+
+<style>
+
+ 
+</style>
